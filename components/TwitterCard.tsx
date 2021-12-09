5bb6adb06d6319ab4b95544e07fe8c6b7fa42794
uenobu106 <uenobu106@gmail.com>
2021-12-09 11:05:12 +0900

    ✅ add props

---
+++ components/TwitterCard.tsx
@@ -0,0 +1,72 @@
+type TwitterCardProps = {
+  user: {
+    name: string;
+    accountName: string;
+    image: string;
+  };
+
+  body: {
+    text: string;
+    image?: string;
+  };
+
+  analytics: {
+    path: string;
+    count: number;
+  }[];
+};
+
+type TweetProps = {
+  type: "tweet";
+};
+
+export const TwitterCard = (props: TwitterCardProps) => {
+  return (
+    <div className="bg-gray-200 pt-20 pb-80 flex items-center justify-center">
+      <div className="bg-white border-gray-200 p-4 rounded-xl border max-w-xl min-w-[480px]">
+        {/* ユーザー */}
+        <div className="flex items-center">
+          <img
+            className="h-11 w-11 rounded-full"
+            src={props.user.image}
+            alt=""
+          />
+          <div className="ml-1.5 text-sm leading-tight">
+            <span className="text-black font-bold block ">
+              {props.user.name}
+            </span>
+            <span className="text-gray-500 font-normal block">{`@${props.user.accountName}`}</span>
+          </div>
+        </div>
+        {/* 本文 */}
+        <p className="text-black block text-xl leading-snug mt-3">
+          {props.body.text}
+        </p>
+        {props.body.image ? (
+          <img
+            className="mt-2 rounded-2xl border border-gray-100"
+            src={props.body.image}
+            alt=""
+          />
+        ) : null}
+        <p className="text-gray-500 text-base py-1 my-0.5">
+          23:30 PM · 2021年12月8日
+        </p>
+        {/* 統計 */}
+        <div className="border-gray-200 border border-b-0 my-1" />
+        <div className="text-gray-500 flex mt-3">
+          {props.analytics.map(({ path, count }) => (
+            <div key={path} className="flex items-center mr-6">
+              <svg className="fill-current h-5 w-auto" viewBox="0 0 24 24">
+                <g>
+                  <path d={path}></path>
+                </g>
+              </svg>
+              <span className="ml-3">{count}</span>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
