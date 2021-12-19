bc8d8e3908009955aa5866fa9008a1ba9a8398d1
uenobu106 <uenobu106@gmail.com>
2021-12-19 19:41:25 +0900

    ✅ react like button tutorial

--- survival-ts/like-button/src/App.tsx
+++ survival-ts/like-button/src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <LikeButton />
       </header>
+      <div className="App-header">TypeScriptはいいぞ</div>
     </div>
   );
 }
 
+function LikeButton() {
+  const [count, setCount] = useState(0);
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+  return (
+    <span className="likeButton" onClick={handleClick}>
+      ♥ {count}
+    </span>
+  );
+}
 export default App;
